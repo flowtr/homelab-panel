04985d5601370b5cb88da93bec4e7bd630afdd58
Theo Paris <theoparisdesigns@gmail.com>
2020-12-09 20:56:45 -0800

    php test

--- src/modules/php-loader/index.ts
+++ src/modules/php-loader/index.ts
@@ -3,11 +3,13 @@ import { parse as parseURL } from "url";
 import { PHPFPM } from "tsnode-phpfpm";
 import path from "upath";
 import { PhpConfiguration } from "./util";
+import { logger } from "../../util/logger";
 export const loader = (config: PhpConfiguration) => {
     const php = new PHPFPM(config);
     return async (req: Request, res: Response) => {
         const url = parseURL(req.url);
         const phpFile = path.basename(url.pathname);
+        logger.debug(`Serving php file: ${phpFile}`);
         php.run(phpFile, (err, output, phpError) => {
             if (err == 99)
                 return res
