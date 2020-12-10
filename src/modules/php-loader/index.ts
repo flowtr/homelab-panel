214bc081fb6c5f2a3d9ed1c49c1f281345c27621
Theo Paris <theoparisdesigns@gmail.com>
2020-12-09 20:53:43 -0800

    php test

--- src/modules/php-loader/index.ts
+++ src/modules/php-loader/index.ts
@@ -10,14 +10,16 @@ export const loader = (config: PhpConfiguration) => {
         const phpFile = path.basename(url.pathname);
         php.run(phpFile, (err, output, phpError) => {
             if (err == 99)
-                res.status(500).send(
-                    `<h1>Encountered PHP FPM Error</h1><p>Code: ${err}</p>`
-                );
+                return res
+                    .status(500)
+                    .send(
+                        `<h1>Encountered PHP FPM Error</h1><p>Code: ${err}</p>`
+                    );
             if (phpError)
-                res.status(500).send(
-                    `<h1>Encountered PHP Error</h1><p>${phpError}</p>`
-                );
-            else res.send(output);
+                return res
+                    .status(500)
+                    .send(`<h1>Encountered PHP Error</h1><p>${phpError}</p>`);
+            return res.send(output);
         });
     };
 };
